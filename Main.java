fe370196241223570a637e66b1616abec62d2e45
jacoe0850 <jacoe0850@wrdsb.ca>
2020-10-07 14:52:36 +0000

    Finished for loop summative

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * takes an integer and tells the sum of all odd numbers up to it
+ * @jacoe0850 
  */
 public class Main {
 
@@ -10,6 +11,19 @@ public class Main {
    */
   public static void main(String[] args) {
     // TODO code application logic here
-    
+    Scanner input = new Scanner(System.in);
+    // ask for an integer and make variable for it
+    System.out.println("Please enter the maximum number to sum to:");
+    int num = input.nextInt();
+    // variable for the sum of all odd numbers
+    int sum = 0;
+    //for loop with if statement
+    for(int i = 0; i <= num; i++){
+      if(i % 2 == 1){
+        sum = sum + i;
+      }
+    }
+    // saying the answer
+    System.out.println("The sum of the odd numbers is" + " " + sum);
   }
 }
